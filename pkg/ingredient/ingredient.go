d92ea5ac5076054225e7df878dd3dc1c6c0535a3
chvck <chvckd@gmail.com>
2017-12-16 15:33:59 +0000

    Add first iteration of the parser interface

--- pkg/ingredient/ingredient.go
+++ pkg/ingredient/ingredient.go
@@ -1,5 +1,8 @@
+// Package ingredient contains the base Ingredient type.
 package ingredient
 
+// Ingredient is the representation of an individual ingredient + info as a part of a recipe.
+// e.g. 100 grams tomatoes, diced.
 type Ingredient struct {
 	unit string
 	name string
@@ -7,38 +10,46 @@ type Ingredient struct {
 	notes string
 }
 
+// SetUnit sets the unit aspect of the ingredient, e.g. gram.
 func (i *Ingredient) SetUnit(unit string) *Ingredient {
 	i.unit = unit
 	return i
 }
 
+// SetName sets the name of the ingredient, e.g. tomato.
 func (i *Ingredient) SetName(name string) *Ingredient {
 	i.name = name
 	return i
 }
 
+// SetQuantity sets the quantity of the ingredient, e.g. 100.
 func (i *Ingredient) SetQuantity(quantity string) *Ingredient {
 	i.quantity = quantity
 	return i
 }
 
+// SetNotes sets any notes for the ingredient, e.g. diced.
 func (i *Ingredient) SetNotes(notes string) *Ingredient {
 	i.notes = notes
 	return i
 }
 
+// Unit gets the unit aspect of the ingredient, e.g. gram.
 func (i *Ingredient) Unit() string {
 	return i.unit
 }
 
+// Name gets the name of the ingredient, e.g. tomato.
 func (i *Ingredient) Name() string {
 	return i.name
 }
 
+// Quantity gets the quantity of the ingredient, e.g. 100.
 func (i *Ingredient) Quantity() string {
 	return i.quantity
 }
 
+// Notes gets any notes for the ingredient, e.g. diced.
 func (i *Ingredient) Notes() string {
 	return i.notes
 }
--- pkg/parser/parser.go
+++ pkg/parser/parser.go
@@ -1,8 +1,11 @@
+// Package parser provides a way to take a string set of ingredients and turn them into
+// an array of Ingredient.
 package parser
 
 import "github.com/chvck/ingredients-parser/pkg/ingredient"
 
+// Parser is the interface that wraps the basic Parse method.
 type Parser interface {
 	SetConfig(config []byte) error
-	ParseIngredients(ingredients string) ([]ingredient.Ingredient, error)
+	Parse(ingredients string) ([]ingredient.Ingredient, error)
 }
