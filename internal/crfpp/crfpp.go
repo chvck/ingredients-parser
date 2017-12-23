048a03870c427aa49a9f82de28f9aeb627c19258
chvck <chvckd@gmail.com>
2017-12-23 06:56:49 +0000

    NO MERGE

--- internal/crfpp/crfpp.go
+++ internal/crfpp/crfpp.go
@@ -7,16 +7,20 @@ import (
 	"fmt"
 )
 
+// ITagger is the interface that wraps crfpp functionality.
 type ITagger interface {
 	Test (modelFilePath string, dataFilePath string) (string, error)
+	Learn (templateFilePath string, trainingFilePath string, modeFilePath string) (string, error)
 }
 
-type Tagger struct{
+// Tagger is the struct that wraps crfpp functionality.
+type Tagger struct {
 
 }
 
 var execCommand = exec.Command
 
+// Test runs crf_test on the supplied data
 func (c Tagger) Test(modelFilePath string, dataFilePath string) (string, error) {
 	cmd := execCommand("crf_test", "-v", "1", "-m", modelFilePath, dataFilePath)
 	crfppOutput, err := execute(cmd)
@@ -27,6 +31,16 @@ func (c Tagger) Test(modelFilePath string, dataFilePath string) (string, error) 
 	return crfppOutput, nil
 }
 
+// Learn runs crf_learn on the supplied data
+func (c Tagger) Learn(templateFilePath string, trainingFilePath string, modeFilePath string) (string, error) {
+	cmd := execCommand("crf_learn", templateFilePath, trainingFilePath)
+	crfppOutput, err := execute(cmd)
+	if err != nil {
+		return "", fmt.Errorf("%s", err.Error())
+	}
+
+	return crfppOutput, nil
+}
 
 // execute executes the given command and returns the output from Stdout.
 func execute(cmd *exec.Cmd) (string, error) {
